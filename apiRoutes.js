2b2b186f91c46c580a235b2af77cbd56d0622c4b
nancykaur11 <nancykaur1119@gmail.com>
2023-12-31 18:10:09 +0530

    update

--- apiRoutes.js
+++ apiRoutes.js
@@ -98,5 +98,98 @@ router.get("/trips", async (req, res) => {
     }
   });
   
+
+  router.post("/orders", async (req, res) => {
+    try {
+      const razorpay = new Razorpay({
+        key_id: "rzp_test_ISZIkTa5BDsapy",
+        key_secret: "cdo8ce5WDCOtRCnsHk6uyMdq",
+      });
+  
+      const options = {
+        amount: req.body.amount * 100,
+        currency: "INR",
+        receipt: crypto.randomBytes(10).toString("hex"),
+      };
+  
+      razorpay.orders.create(options, (error, order) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({ message: "Something Went Wrong!" });
+        }
+        res.status(200).json({ data: order });
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error!" });
+      console.log(error);
+    }
+  });
+  
+  router.post("/verify", async (req, res) => {
+    try {
+      const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+        req.body;
+      const sign = razorpay_order_id + "|" + razorpay_payment_id;
+      const expectedSign = crypto
+        .createHmac("sha256", process.env.KEY_SECRET)
+        .update(sign.toString())
+        .digest("hex");
+  
+      if (razorpay_signature === expectedSign) {
+        return res.status(200).json({ message: "Payment verified successfully" });
+      } else {
+        return res.status(400).json({ message: "Invalid signature sent!" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error!" });
+      console.log(error);
+    }
+  });
+  const Order = mongoose.model(
+    "Order",
+    new mongoose.Schema({
+      orderId: String,
+      paymentId: String,
+      amount: Number,
+      currency: String,
+      status: String,
+    })
+  );
+  
+  router.post("/create-order", async (req, res) => {
+    const payment_capture = 1;
+    const amount = 500;
+    const currency = "INR";
+  
+    const options = {
+      amount: amount * 100,
+      currency,
+      receipt: "receipt#1",
+      payment_capture,
+    };
+  
+    try {
+      const response = await razorpay.orders.create(options);
+      let order = new Order({
+        orderId: response.id,
+        amount: response.amount,
+        currency: response.currency,
+        status: "created",
+      });
+      await order.save();
+      res.json({
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount,
+      });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  router.post("/verify-payment", async (req, res) => {
+    const { payment_id, order_id } = req.body;
+    res.json({ status: "Payment verified successfully" });
+  });
   
 module.exports = router;
